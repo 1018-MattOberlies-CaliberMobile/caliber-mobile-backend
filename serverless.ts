6b80d42b865336433ca0e17a3b7cd1e4993bc491
DustinDiazLopez <hunterghost526@gmail.com>
2021-07-28 14:38:15 -0500

    broken sls config

--- serverless.ts
+++ serverless.ts
@@ -5,6 +5,9 @@ import hello from '@functions/hello';
 const serverlessConfiguration: AWS = {
   service: 'caliber-mobile-backend',
   frameworkVersion: '2',
+  package: {
+    individually: true,
+  },
   custom: {
     esbuild: {
       bundle: true,
@@ -22,6 +25,7 @@ const serverlessConfiguration: AWS = {
   plugins: ['serverless-esbuild', 'serverless-plugin-resource-tagging'],
   provider: {
     name: 'aws',
+    region: 'us-east-1',
     runtime: 'nodejs14.x',
     apiGateway: {
       minimumCompressionSize: 1024,
@@ -31,39 +35,64 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
     lambdaHashingVersion: '20201221',
+    stackTags: {
+      'Created By': '1018-MattOberlies-CaliberMobile',
+      'Delete After': 'never',
+      'Contact Before Delete': 'matthew',
+      'Purpose': 'Backend for Caliber Mobile',
+    },
+    iam: {
+
+    }
   },
   // import the function via paths
   functions: { hello },
   resources: {
     Resources: {
-
-      "DBSecurityGroup": {
-        "Type": "AWS::RDS::DBSecurityGroup",
+      WebServerSecurityGroup: {
+        "Type": "AWS::EC2::SecurityGroup",
         "Properties": {
-          "DBSecurityGroupIngress": { "EC2SecurityGroupName": { "Ref": "WebServerSecurityGroup" } },
+          "GroupDescription": "Enable HTTP access via port 80, SSH access, and PostgreSQL access",
+          "SecurityGroupIngress": [
+            { "IpProtocol": "tcp", "FromPort": "80", "ToPort": "80", "CidrIp": "0.0.0.0/0" },
+            { "IpProtocol": "tcp", "FromPort": "22", "ToPort": "22", "CidrIp": "0.0.0.0/0" },
+            { "IpProtocol": "tcp", "FromPort": "5432", "ToPort": "5432", "CidrIp": "0.0.0.0/0" },
+          ],
+        },
+      },
+      why: {
+        Type: "AWS::RDS::DBSecurityGroup",
+        Properties: {
+          DBSecurityGroupIngress: {
+            "EC2SecurityGroupName": {
+              Ref: "WebServerSecurityGroup"
+            }
+          },
           "GroupDescription": "Frontend Access"
         }
       },
+      
       caliberMobileDB: {
         Type: 'AWS::RDS::DBInstance',
         Properties: {
-          'DBSecurityGroups': [
+          
+          DBSecurityGroups: [
             {
-              'Ref': 'MyDbSecurityByEC2SecurityGroup'
+              Ref: 'why'
             },
             {
-              'Ref': 'MyDbSecurityByCIDRIPGroup'
+              Ref: 'WebServerSecurityGroup'
             }
           ],
-          AllocatedStorage: '5',
-          DBInstanceClass: 'db.m1.small',
-          Engine: 'PostgreSQL',
-          MasterUsername: 'MyName',
-          MasterUserPassword: 'MyPassword',
+          AllocatedStorage: '20',
+          DBInstanceClass: 'db.t2.micro',
+          Engine: 'postgres',
+          MasterUsername: 'postgres',
+          MasterUserPassword: 'password',
         },
       },
     },
   },
 };
 
-export default serverlessConfiguration;
+module.exports = serverlessConfiguration;
