883d3319bc16136ce5c3d057ca447f879d22e319
adidde1073 <84734004+adidde1073@users.noreply.github.com>
2021-07-30 11:13:51 -0700

    Select wk ad (#2)
    * Created DAOs folder and models folder in src; creplicated models and added DAOs for batch and note
    
    * added jest and mock data
    
    * moved jest to devDependencies
    
    Co-authored-by: Albert Didde <albertdidde@Alberts-MacBook-Pro.local>

---
+++ src/models/user.ts
@@ -0,0 +1,12 @@
+/* eslint-disable semi */
+/* eslint-disable no-extra-semi */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable no-unused-vars */
+
+import { Role } from '../@types';
+
+export default interface User {
+    userId: string,
+    role: Role,
+};
