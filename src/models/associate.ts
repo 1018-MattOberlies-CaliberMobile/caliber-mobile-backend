219fd296a543d9aaf90d199cc37d76b7a05be5c6
Albert Didde <albertdidde@Alberts-MacBook-Pro.local>
2021-07-30 09:43:13 -0700

    Created DAOs folder and models folder in src; creplicated models and added DAOs for batch and note

---
+++ src/models/associate.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-extra-semi */
+/* eslint-disable semi */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable no-unused-vars */
+
+export default interface Associate {
+    associateId: string,
+    firstName: string,
+    lastName: string,
+  };
