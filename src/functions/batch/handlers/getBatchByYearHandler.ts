c60bcde942df315921747f2dac5386a14a26d1e5
Sam Henley <samueljhenley@gmail.com>
2021-08-03 12:34:03 -0400

    develop getBatchYears lambda

--- src/functions/batch/handlers/getBatchByYearHandler.ts
+++ src/functions/batch/handlers/getBatchByYearHandler.ts
@@ -10,7 +10,7 @@ import Batch from '../../../models/batch';
 const getBatchByYearHandler: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
   const year = event.path.split('/').pop();
   const batches: Batch[] = (await Models.default.Batch.findAll()).map((response) => response.get());
-
+  
   // This would be better, but it doesnt work :(
   // const res = batches.filter((batch) => {
   //   new Date(batch.startDate).toISOString().split('-').shift() === year;
