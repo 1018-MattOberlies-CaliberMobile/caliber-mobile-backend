9ef9e897036ca0a661d014e4ee8674902278e3cc
dixond1097 <79214021+dixond1097@users.noreply.github.com>
2021-08-03 16:02:58 -0400

    Created createNoteHandler Lambda function

--- src/functions/note/handlers/createNoteHandler.ts
+++ src/functions/note/handlers/createNoteHandler.ts
@@ -3,14 +3,14 @@ import 'source-map-support/register';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-
+import Models from '../../../repositories/models';
 import schema from '../schemas/createNoteSchema';
 
 const createNoteHandler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const note = await Models.Note.create(event.body);
   console.log('filler');
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
+    message: note,
   });
 };
 
