20f6c6e179e6dfffff267ea11c9a111e6eff0c6a
Sam Henley <samueljhenley@gmail.com>
2021-08-03 17:43:55 -0400

    refactor db imports (#10)
    * add skeleton for some batch DAO functions, modify some model attributes
    
    * install @types/jest, set up jest
    
    * install sequelize dependencies
    
    * install ts-jest, write basic unit tests for getBatchYears and getBatchesByYear
    
    * develop getBatchByYear lambda
    
    need to do role check
    
    * develop getBatchYears lambda
    
    * make db imports cleaner

--- src/functions/batch/handlers/getBatchByYearHandler.ts
+++ src/functions/batch/handlers/getBatchByYearHandler.ts
@@ -3,13 +3,12 @@ import 'source-map-support/register';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-import * as Models from '../../../repositories/models';
+import db from '../../../repositories/models';
 import Batch from '../../../models/batch';
-
 
 const getBatchByYearHandler: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
   const year = event.path.split('/').pop();
-  const batches: Batch[] = (await Models.default.Batch.findAll()).map((response) => response.get());
+  const batches: Batch[] = (await db.Batch.findAll()).map((response) => response.get());
   
   // This would be better, but it doesnt work :(
   // const res = batches.filter((batch) => {
--- src/functions/batch/handlers/getBatchYearsHandler.ts
+++ src/functions/batch/handlers/getBatchYearsHandler.ts
@@ -3,11 +3,11 @@ import 'source-map-support/register';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-import * as Models from '../../../repositories/models';
+import db from '../../../repositories/models';
 
 
 const getBatchYearsHandler: ValidatedEventAPIGatewayProxyEvent<unknown> = async () => {
-  const years: string[] = (await Models.default.Batch.findAll({
+  const years: string[] = (await db.Batch.findAll({
     attributes: ['startDate'],
   })).map((response) => {
     const date = response.getDataValue('startDate');
