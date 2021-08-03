3058b8adbc29369c76449de566eaafc14a9ec525
Sam Henley <samueljhenley@gmail.com>
2021-08-03 12:17:10 -0400

    develop getBatchByYear lambda
    need to do role check

--- src/functions/batch/handlers/getBatchByYearHandler.ts
+++ src/functions/batch/handlers/getBatchByYearHandler.ts
@@ -3,12 +3,30 @@ import 'source-map-support/register';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
+import * as Models from '../../../repositories/models';
+import Batch from '../../../models/batch';
+
 
 const getBatchByYearHandler: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
-  console.log('filler');
+  const year = event.path.split('/').pop();
+  const batches: Batch[] = (await Models.default.Batch.findAll()).map((response) => response.get());
+
+  // This would be better, but it doesnt work :(
+  // const res = batches.filter((batch) => {
+  //   new Date(batch.startDate).toISOString().split('-').shift() === year;
+  // });
+
+  // Need to check the role of the user making the request. If a trainer, should only return batches for which
+  // they are a trainer for
+  const res = [];
+  batches.forEach((batch) => {
+    if(new Date(batch.startDate).getFullYear().toString() === year) {
+      res.push(batch);
+    }
+  });
+
   return formatJSONResponse({
-    message: 'Hello notes by batch id, welcome to the exciting Serverless world!',
-    event,
+    res,
   });
 };
 
