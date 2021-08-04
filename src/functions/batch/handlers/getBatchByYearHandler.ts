393c0cbed57f6d225fad0eb416006213462002c8
Sam Henley <samueljhenley@gmail.com>
2021-08-04 16:51:16 -0400

    refactor getBatchByYear lambda to check user role

--- src/functions/batch/handlers/getBatchByYearHandler.ts
+++ src/functions/batch/handlers/getBatchByYearHandler.ts
@@ -5,28 +5,55 @@ import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 import db from '../../../repositories/models';
 import Batch from '../../../models/batch';
+import * as AWS from 'aws-sdk';
+import { GetUserResponse } from 'aws-sdk/clients/cognitoidentityserviceprovider';
 
 const getBatchByYearHandler: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
   const year = event.path.split('/').pop();
-  const batches: Batch[] = (await db.Batch.findAll()).map((response) => response.get());
+  const AccessToken = event.headers.get['Authorization'].split(' ').pop();
+  
+  const cognito = new AWS.CognitoIdentityServiceProvider({region: 'us-west-1'});
+
+  cognito.getUser({ AccessToken, }, async (err: AWS.AWSError, res: GetUserResponse) => {
+    if(err) {
+      return formatJSONResponse({ statusCode: 403, body: ''});
+    }
+    const batches: Batch[] = (await db.Batch.findAll()).map((response) => response.get());
+
+    const userRole = res.UserAttributes.find( (attr) => attr.Name === 'custom:role').Value;
+    const username = res.Username;
+    
+    const returnBatches = [];
+
+    batches.forEach((batch) => {
+      if ((userRole === 'Trainer' && batch.trainers.find((trainer) => trainer.username === username))) {
+        if(new Date(batch.startDate).getFullYear().toString() === year) {
+          returnBatches.push(batch);
+        }
+      } else {
+        if(new Date(batch.startDate).getFullYear().toString() === year) {
+          returnBatches.push(batch);
+        }
+      }
+      
+    });
+
+    return formatJSONResponse({
+      statusCode: 200,
+      body: returnBatches,
+    });
+  
+  });
+
+  return formatJSONResponse({
+    statusCode: 500,
+  });
   
   // This would be better, but it doesnt work :(
   // const res = batches.filter((batch) => {
   //   new Date(batch.startDate).toISOString().split('-').shift() === year;
   // });
-
-  // Need to check the role of the user making the request. If a trainer, should only return batches for which
-  // they are a trainer for
-  const res = [];
-  batches.forEach((batch) => {
-    if(new Date(batch.startDate).getFullYear().toString() === year) {
-      res.push(batch);
-    }
-  });
-
-  return formatJSONResponse({
-    res,
-  });
+  
 };
 
 export const main = middyfy(getBatchByYearHandler);
