ad93d86b7c55e48f1fec62f8d23635da781ee450
DustinDiazLopez <hunterghost526@gmail.com>
2021-08-07 22:10:17 -0500

    added to test a specific file

---
+++ testfile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export DATABASE_USERNAME=postgres
+export DATABASE_PASSWORD=password
+export DATABASE_HOST=
+export DATABASE_DIALECT=postgres 
+export TESTING=1
+npx jest --coverage -- "$@"
