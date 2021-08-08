2dcfc111aaeaa0230ee8b99b017fc2890b4babce
Kyle Bartolucci <bartoluccik@gmail.com>
2021-08-07 17:19:04 -0700

    modified note.dao.test to include null case

--- src/repositories/noteDAO/note.dao.test.ts
+++ src/repositories/noteDAO/note.dao.test.ts
@@ -1,14 +1,21 @@
 /* eslint-disable dot-notation */
-import noteDao from './note.dao';
+import NoteDAO from './note.dao';
 
 describe('note DAO', () => {
   describe('getNoteWeek', () => {
     test('returns a list of notes for a given week', async () => {
       const weekNumber = Math.floor(Math.random() * 10);
       const batchId = '123-456-789';
-      const noteList = (await noteDao.getNotesByBatchAndWeek(batchId, weekNumber));
+      const noteList = (await NoteDAO.getNotesByBatchAndWeek(batchId, weekNumber));
+      console.log(noteList);
       const returnedWeek = noteList[0]['weekNumber'];
       expect(returnedWeek).toEqual(weekNumber);
+      
+    });
+    test('returns null with invalid data', async () => {
+      const batchId = '123-456-789';
+      const noteList = (await NoteDAO.getNotesByBatchAndWeek(batchId, 0));
+      expect(noteList).toEqual(null);
     });
   });
 });
