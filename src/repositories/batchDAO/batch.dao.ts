04d78897864587dfc20df3f8cdcce1410c539172
Sam Henley <samueljhenley@gmail.com>
2021-08-08 16:12:09 -0400

    batch dao unit tests (#18)
    * add skeleton for some batch DAO functions, modify some model attributes
    
    * install @types/jest, set up jest
    
    * install sequelize dependencies
    
    * install ts-jest, write basic unit tests for getBatchYears and getBatchesByYear
    
    * develop getBatchByYear lambda
    
    need to do role check
    
    * develop getBatchYears lambda
    
    * make db imports cleaner
    
    * Fixed sls
    
    * updated .env.example and serverless.ts
    
    * refactor getBatchByYear lambda to check user role
    
    * reorganize
    
    * Fixed lambda endpoint
    
    * make getUser async, finish lambda functionality
    
    * change username to string instead of UUID
    
    * fix logic for checking trainer
    
    * refactor getBatchesByYear to use batch DAO
    
    * refactor getBatchYears to use batch DAO
    
    * start work on batch unit tests
    
    * write tests for getBatchYears
    
    * finish writing unit tests for batch DAO
    
    Co-authored-by: DustinDiazLopez <hunterghost526@gmail.com>

--- src/repositories/batchDAO/batch.dao.ts
+++ src/repositories/batchDAO/batch.dao.ts
@@ -4,10 +4,11 @@ import CognitoClient from '../../util/cognito';
 
 class BatchDAO {
   static async getBatchYears(): Promise<string[]> {
-    const years: string[] = (await db.Batch.findAll({
+    const years = (await db.Batch.findAll({
       attributes: ['startDate'],
     })).map((response) => {
-      const date = response.getDataValue('startDate');
+      const res = JSON.parse(JSON.stringify(response));
+      const date = res.startDate;
       return new Date(date).getFullYear().toString();
     });
 
@@ -25,7 +26,7 @@ class BatchDAO {
         model: db.User,
         as: 'users',
       },
-    })).map((response) => response.get());
+    })).map((response) => JSON.parse(JSON.stringify(response)));
 
     const returnBatches = [];
 
