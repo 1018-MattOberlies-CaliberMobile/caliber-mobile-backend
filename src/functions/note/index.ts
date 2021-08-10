c3b777b1f7348411e6a215339ad752de1b2e0a0e
dallinlemon <dallinlemon@gmail.com>
2021-08-10 12:53:00 -0600

    added int lambda

--- src/functions/note/index.ts
+++ src/functions/note/index.ts
@@ -18,6 +18,7 @@ export const createNote = {
         method: 'post',
         path: note,
         cors: true,
+        integration: 'lambda',
         request: {
           schemas: {
             'application/json': createNoteSchema,
